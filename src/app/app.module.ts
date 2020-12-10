7814317744b7f1c03c646779de0a91376f3191eb
sumanmandal <suman.mandal@obopay.com>
2020-12-10 17:05:05 +0530

    add charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule }        from '@angular/platform-browser';
-import { NgModule }             from '@angular/core';
-import { AppRoutingModule }     from './app-routing.module';
-import { AppComponent }         from './app.component';
-import { SectionOneComponent }  from './section-one/section-one.component';
-import { FlexLayoutModule}      from '@angular/flex-layout';
-import { SectionTwoComponent } from './section-two/section-two.component';
-import { FooterComponent } from './footer/footer.component';
+import { BrowserModule }          from '@angular/platform-browser'
+import { NgModule }               from '@angular/core'
+import { AppRoutingModule }       from './app-routing.module'
+import { AppComponent }           from './app.component'
+import { SectionOneComponent }    from './section-one/section-one.component'
+import { FlexLayoutModule}        from '@angular/flex-layout'
+import { SectionTwoComponent }    from './section-two/section-two.component'
+import { FooterComponent }        from './footer/footer.component'
+import { SectionSkillComponent }  from './section-skill/section-skill.component'
+import { ChartsModule }           from 'ng2-charts'
 
 @NgModule({
   declarations: [
     AppComponent,
     SectionOneComponent,
     SectionTwoComponent,
-    FooterComponent
+    FooterComponent,
+    SectionSkillComponent
   ],
   imports: [
     FlexLayoutModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
